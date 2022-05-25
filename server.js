77c853aa8fd97ac1c2f0c61b04452577c7c90ae8
)- <a-@-s-MacBook-Pro.local>
2022-05-25 16:09:35 -0400

    food menu added

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const drinks = require('./models/drinks')
+const food = require('./models/food')
 
 const app = express();
 const port = 3000;
 //Routes
 app.get('/drinks/', (req, res) => {
-    res.render('drinks_index.ejs', {drink: drinks});
+    res.render('index.ejs', {drink: drinks, food: food});
 })
-
-
-app.get('/drinks/', (req, res) => {
-    res.render('drinks_index.ejs', {drink: drinks})
-})
-
 
 app.get('/drinks/:indexOfDrinksArray', (req, res) => {
     res.render('show.ejs',{
         drink: drinks[req.params.indexOfDrinksArray]
+    })
+})
+
+app.get('/food/:id', (req, res) => {
+    res.render('showFood.ejs', {
+        food: food[req.params.id]
     })
 })
 
