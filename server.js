2738cd57c8a3614d57bd0b861582a5e23cbdf977
)- <a-@-s-MacBook-Pro.local>
2022-05-25 12:16:45 -0400

    drink menu displays dynamically by ejs on our index.ejs views

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const drinks = require('./models/drinks')
 
 const app = express();
 const port = 3000;
+//Routes
+app.get('/drinks/', (req, res) => {
+    res.render('drinks_index.ejs', {drink: drinks});
+})
 
-//Index
+
 app.get('/drinks/', (req, res) => {
     res.render('drinks_index.ejs', {drink: drinks})
 })
 
-app.get('/index', (req,res) => {
-    res.render('index.ejs', {drink: drinks})
-})
 
 app.get('/drinks/:indexOfDrinksArray', (req, res) => {
     res.render('show.ejs',{
         drink: drinks[req.params.indexOfDrinksArray]
     })
 })
-
 
 app.listen(port, () => {
     console.log(`listening on `, port)
